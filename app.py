c9da002d02433fd14890ffa6221850480cac4e54
Ilya Shabanov <ilya@one-extra.de>
2024-05-31 17:33:51 +1200

    test

--- app.py
+++ app.py
@@ -5,39 +5,42 @@ import gunicorn                     #whilst your local machine's webserver doesn
 from whitenoise import WhiteNoise   #for serving static files on Heroku
 
 # Instantiate dash app
-app = Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
-
-# Reference the underlying flask app (Used by gunicorn webserver in Heroku production deployment)
-server = app.server 
-
-# Enable Whitenoise for serving static files from Heroku (the /static folder is seen as root by Heroku) 
-server.wsgi_app = WhiteNoise(server.wsgi_app, root='static/') 
-
 
 app = Dash(__name__, assets_folder='./assets', use_pages=True, pages_folder="./src/pages",
            suppress_callback_exceptions=True)
 
-app.layout = html.Div([
-    html.Nav([
-        html.Div(className="nav-container", children=[
-            html.Div(className="nav-logo", children='ATI Results Overview'),
-            html.Div(className="nav-links", children=
-            [
-                html.Div(
-                    dcc.Link(f"{page['name']}", href=page["relative_path"])
-                )
-                for page in dash.page_registry.values()
-            ]
-                     ),
-        ]),
-    ]),
-    html.Div(id="overlay", className="page-content", children=[
-        html.Div(className="text", children= ['Running...']),
-    ]),
-    dash.page_container
-])
+# Reference the underlying flask app (Used by gunicorn webserver in Heroku production deployment)
+server = app.server
 
+# Enable Whitenoise for serving static files from Heroku (the /static folder is seen as root by Heroku)
+server.wsgi_app = WhiteNoise(server.wsgi_app, root='static/')
+
+def create_dash_layout(app):
+
+    app.layout = html.Div([
+        html.Nav([
+            html.Div(className="nav-container", children=[
+                html.Div(className="nav-logo", children='ATI Results Overview'),
+                html.Div(className="nav-links", children=
+                [
+                    html.Div(
+                        dcc.Link(f"{page['name']}", href=page["relative_path"])
+                    )
+                    for page in dash.page_registry.values()
+                ]
+                         ),
+            ]),
+        ]),
+        html.Div(id="overlay", className="page-content", children=[
+            html.Div(className="text", children= ['Running...']),
+        ]),
+        dash.page_container
+    ])
+
+    return app
+
+app = create_dash_layout(app)
 
 # Run flask app
 if __name__ == "__main__":
-    app.run_server(debug=True, host='0.0.0.0', port=8050)
+    app.run_server(debug=False, host='0.0.0.0', port=8050)
